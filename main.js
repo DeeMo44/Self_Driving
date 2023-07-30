d664f3d4127bfb62fa8833b31fcb05496a1b8b9b
DeeMo44 <demario326@gmail.com>
2023-07-30 03:58:09 -0400

    complete

--- main.js
+++ main.js
@@ -8,22 +8,22 @@ const networkCtx = networkCanvas.getContext('2d');
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9)
 
-const N = 300
+const N = 600
 const cars = generateCars(N)
 let bestCar = cars[0]
 if (localStorage.getItem("bestBrain")) {
-  console.log(localStorage)
   for (let i = 0; i < cars.length; i++) {
     cars[i].brain = JSON.parse(
       localStorage.getItem("bestBrain")
     )
-    if(i!=0){
-      NeuralNetwork.mutate(cars[i].brain,0.05)
+    if (i != 0) {
+      NeuralNetwork.mutate(cars[i].brain, 0.15)
     }
   }
 
 }
 
+let lastTrafficSpawnTime = 0;
 // standard cars before random cars
 const traffic = [
   new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
@@ -35,38 +35,17 @@ const traffic = [
   new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2)
 ]
 
-const maxDummyCars = 4
-function generateRandomNegativeNumber(min, max) {
-  return -Math.random() * (max - min) - min;
-}
-function generateDummyCar(x, y) {
-  return new Car(x, y, 30, 50, "DUMMY", 2);
-}
-function spawnDummyCar(aiYPosition, minDummyDistance, minAICarDistance, maxDummyCars) {
-  if (traffic.length >= maxDummyCars) {
-    return; // Don't spawn more cars if the maximum limit has been reached
-  }
-  const randomLane = Math.floor(Math.random() * 3); // Assuming there are 3 lanes (0, 1, 2)
-  let randomYOffset = generateRandomNegativeNumber(100, 1000); // Adjust the range as needed
-  while (Math.abs(randomYOffset) < minDummyDistance || Math.abs(randomYOffset + aiYPosition) < minAICarDistance) {
-    // Ensure a minimum distance between dummy cars and between the AI car and the closest dummy car
-    randomYOffset = generateRandomNegativeNumber(100, 1000);
-  }
-
-  const dummyY = aiYPosition + randomYOffset;
-  traffic.push(generateDummyCar(road.getLaneCenter(randomLane), dummyY));
-}
-
 animate();
 
 function save() {
   localStorage.setItem("bestBrain",
     JSON.stringify(bestCar.brain)
   )
+  location.reload();
 }
-
 function discard() {
   localStorage.removeItem("bestBrain")
+  location.reload();
 }
 
 function generateCars(N) {
@@ -78,34 +57,38 @@ function generateCars(N) {
 }
 
 function animate(time) {
-
-  
   // Update and spawn dummy cars
   for (let i = traffic.length - 1; i >= 0; i--) {
     traffic[i].update(road.borders, []);
-    
-    // If a dummy car goes below the canvas, remove it from the array
-    if (traffic[i].y > carCanvas.height) {
-      traffic.splice(i, 1);
-    }
   }
-
-  const aiYPosition = bestCar.y; // Store the AI car's Y position
-  const minDummyDistance = 500; // Adjust the minimum distance between dummy cars as needed
-  const minAICarDistance = 1000; // Adjust the minimum distance between the AI car and the closest dummy car as needed
-
-  // Spawn new dummy car every X frames (you can adjust this as needed)
-  const spawnFrequency = 100;
-  if (Math.random() < 1 / spawnFrequency) {
-    spawnDummyCar(aiYPosition, minDummyDistance, minAICarDistance, maxDummyCars); // Pass the required parameters
-  }
-
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].update(road.borders, [])
   }
   for (let i = 0; i < cars.length; i++) {
     cars[i].update(road.borders, traffic);
   }
+
+  // Calculate the time difference since the last traffic spawn
+  const timeDiff = time - lastTrafficSpawnTime;
+
+  // Check if one second has passed since the last traffic spawn
+  if (timeDiff >= 3000) {
+    // Spawn a new "DUMMY" traffic at a random lane
+    const randomLane = Math.floor(Math.random() * 3);
+    const newTraffic = new Car(road.getLaneCenter(randomLane), (bestCar.y - 900), 30, 50, "DUMMY", 2);
+    traffic.push(newTraffic);
+
+  // Decide whether to spawn a second car (double)
+  const shouldSpawnDouble = Math.random() < 2 / 3; // 2 out of 3 lanes have a chance of double car
+  if (shouldSpawnDouble) {
+    const randomLaneDouble = Math.floor(Math.random() * 3);
+    const newTrafficDouble = new Car(road.getLaneCenter(randomLaneDouble), bestCar.y - 900, 30, 50, "DUMMY", 2);
+    traffic.push(newTrafficDouble);
+  }
+    // Update the last traffic spawn time
+    lastTrafficSpawnTime = time;
+  }
+
   bestCar = cars.find(
     c => c.y == Math.min(
       ...cars.map(c => c.y)
@@ -114,10 +97,8 @@ function animate(time) {
 
   carCanvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight;
-
   carCtx.save();
   carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7)
-
   road.draw(carCtx);
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].draw(carCtx, 'red')
@@ -128,9 +109,7 @@ function animate(time) {
   }
   carCtx.globalAlpha = 1
   bestCar.draw(carCtx, 'green', true);
-
   carCtx.restore();
-
   networkCtx.lineDashOffset = -time / 50
   Visualizer.drawNetwork(networkCtx, bestCar.brain)
   requestAnimationFrame(animate);
